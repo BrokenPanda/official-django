5c81ac69b185affeb48fb1ace0ca8ae15c155377
Brokenpanda <35952556+BrokenPanda@users.noreply.github.com>
2020-03-04 13:56:52 +0100

    templates added, fixed linting

--- poll/urls.py
+++ poll/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # /poll/
+    path('', views.index, name='index'),
+    # /poll/2/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # /poll/2/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # /poll/2/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
